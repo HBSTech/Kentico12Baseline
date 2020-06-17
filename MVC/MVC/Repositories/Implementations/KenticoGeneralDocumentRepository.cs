0d8107866e8dd668f4f24c6eadda81faa94b5efb
Trevor Fayas <tfayas@hbs.net>
2020-06-17 13:56:39 -0500

    Add missing dependency and adjust to multidocumentquery

--- MVC/MVC/Repositories/Implementations/KenticoGeneralDocumentRepository.cs
+++ MVC/MVC/Repositories/Implementations/KenticoGeneralDocumentRepository.cs
@@ -24,73 +24,72 @@ namespace Generic.Repositories.Implementations
         }
 
         [CacheDependency("node|##SITENAME##|{0}|childnodes")]
+        [CacheDependency("node|##SITENAME##|{0}")]
         public IEnumerable<ITreeNode> GetDocuments(string SinglePath, PathSelectionEnum PathType, string[] PageTypes = null, string OrderBy = null, string WhereCondition = null, int MaxLevel = -1, int TopNumber = -1, string[] Columns = null, bool IncludeCoupledColumns = false)
         {
-            DocumentQuery Query = null;
-            if(PageTypes != null && PageTypes.Length > 0)
+            using (MultiDocumentQuery Query = new MultiDocumentQuery())
             {
-                if(PageTypes.Length == 1)
+                if (PageTypes != null && PageTypes.Length > 0)
                 {
-                    Query = new DocumentQuery(PageTypes[0]);
-                } else
-                {
-                    Query = new DocumentQuery()
-                        .WhereIn("ClassName", PageTypes);
-                    if (IncludeCoupledColumns)
+                    if (PageTypes.Length == 1)
                     {
-                        Query.ExpandColumns();
+                        Query.Type(PageTypes[0]);
+                    }
+                    else
+                    {
+                        Query.Types(PageTypes);
                     }
                 }
-            } else
-            {
-                Query = new DocumentQuery();
-            }
+                if (IncludeCoupledColumns)
+                {
+                    Query.ExpandColumns();
+                }
 
-            // Handle culture and versioning and site
-            Query.Culture(cultureName)
-                .CombineWithDefaultCulture()
-                .CombineWithAnyCulture()
-                .Published(!latestVersionEnabled)
-                .LatestVersion(latestVersionEnabled)
-                .OnSite(_SiteRepo.CurrentSiteName());
+                // Handle culture and versioning and site
+                Query.Culture(cultureName)
+                    .CombineWithDefaultCulture()
+                    .CombineWithAnyCulture()
+                    .Published(!latestVersionEnabled)
+                    .LatestVersion(latestVersionEnabled)
+                    .OnSite(_SiteRepo.CurrentSiteName());
 
-            PathTypeEnum KenticoPathType = PathTypeEnum.Explicit;
-            switch(PathType)
-            {
-                case PathSelectionEnum.ChildrenOnly:
-                    KenticoPathType = PathTypeEnum.Children;
-                    break;
-                case PathSelectionEnum.ParentAndChildren:
-                    KenticoPathType = PathTypeEnum.Section;
-                    break;
-                case PathSelectionEnum.ParentOnly:
-                    KenticoPathType = PathTypeEnum.Single;
-                    break;
-            }
-            Query.Path(SinglePath, KenticoPathType);
+                PathTypeEnum KenticoPathType = PathTypeEnum.Explicit;
+                switch (PathType)
+                {
+                    case PathSelectionEnum.ChildrenOnly:
+                        KenticoPathType = PathTypeEnum.Children;
+                        break;
+                    case PathSelectionEnum.ParentAndChildren:
+                        KenticoPathType = PathTypeEnum.Section;
+                        break;
+                    case PathSelectionEnum.ParentOnly:
+                        KenticoPathType = PathTypeEnum.Single;
+                        break;
+                }
+                Query.Path(SinglePath, KenticoPathType);
 
-            if(!string.IsNullOrWhiteSpace(OrderBy))
-            {
-                Query.OrderBy(OrderBy);
+                if (!string.IsNullOrWhiteSpace(OrderBy))
+                {
+                    Query.OrderBy(OrderBy);
+                }
+                if (!string.IsNullOrWhiteSpace(WhereCondition))
+                {
+                    Query.Where(WhereCondition);
+                }
+                if (Columns != null && Columns.Length > 0)
+                {
+                    Query.Columns(Columns);
+                }
+                if (MaxLevel >= 0)
+                {
+                    Query.NestingLevel(MaxLevel);
+                }
+                if (TopNumber >= 0)
+                {
+                    Query.TopN(TopNumber);
+                }
+                return Query.TypedResult;
             }
-            if (!string.IsNullOrWhiteSpace(WhereCondition))
-            {
-                Query.Where(WhereCondition);
-            }
-            if (Columns != null && Columns.Length > 0)
-            {
-                Query.Columns(Columns);
-            }
-            if (MaxLevel >= 0)
-            {
-                Query.NestingLevel(MaxLevel);
-            }
-            if (TopNumber >= 0)
-            {
-                Query.TopN(TopNumber);
-            }
-
-            return Query.TypedResult;
         }
 
         [CacheDependency("nodeguid|##SITENAME##|{0}")]
